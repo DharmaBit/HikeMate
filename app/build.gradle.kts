ef8baf104807d9acfa6be9543ad7eaee71b8cd62
DharmaBit <dharmabitdev@gmail.com>
2025-10-06 22:43:20 +0700

    x1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.dharmabit.hikemate"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.dharmabit.hikemate"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0.0"
 
